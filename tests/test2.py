24be079ea72d483670aa605ccdd43b68ee2a1cbf
Filip <filip.wozniak99@gmail.com>
2020-11-03 23:19:54 +0100

    red/test1024

--- tests/test2.py
+++ tests/test2.py
@@ -72,9 +72,9 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_900_being_1000_100_is_cm(self):
         self.assertEqual(self.temp.roman(911), "CMXI")
 
-    @unittest.SkipTest
+
     def test_1000_is_a_single_m(self):
-        self.assertEqual(roman(1024), "MXXIV")
+        self.assertEqual(self.temp.roman(1024), "MXXIV")
 
     @unittest.SkipTest
     def test_3000_is_three_m_s(self):
