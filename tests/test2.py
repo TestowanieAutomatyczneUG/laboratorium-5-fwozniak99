b61f48f28fe629d92084baa63e5be6847b55cc60
Filip <filip.wozniak99@gmail.com>
2020-11-03 22:23:40 +0100

    red/test3

--- tests/test2.py
+++ tests/test2.py
@@ -12,9 +12,9 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_2_is_two_i_s(self):
         self.assertEqual(self.temp.roman(2), "II")
 
-    @unittest.SkipTest
+
     def test_3_is_three_i_s(self):
-        self.assertEqual(roman(3), "III")
+        self.assertEqual(self.temp.roman(3), "III")
 
     @unittest.SkipTest
     def test_4_being_5_1_is_iv(self):
